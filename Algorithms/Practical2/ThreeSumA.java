1dbf52c7e5231831f764d1f4c168ed8fac808ce6
Adam Leacy <48072343+aleacy0908@users.noreply.github.com>
2020-04-25 18:55:04 +0100

    Added practical 2 files

---
+++ Algorithms/Practical2/ThreeSumA.java
@@ -0,0 +1,74 @@
+package Practical2; /******************************************************************************
+ *  Compilation:  javac ThreeSumA.java
+ *  Execution:    java ThreeSum input.txt
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ ******************************************************************************/
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreeSumA {
+
+    // Do not instantiate.
+    private ThreeSumA() { }
+
+
+
+    /**
+     * Returns the number of triples (i, j, k) with {@code i < j < k}
+     * such that {@code a[i] + a[j] + a[k] == 0}.
+     *
+     * @param  a the array of integers
+     * @return the number of triples (i, j, k) with {@code i < j < k}
+     *         such that {@code a[i] + a[j] + a[k] == 0}
+     */
+    public static int count(int[] a) {
+        int n = a.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int k = j+1; k < n; k++) {
+                    if (a[i] + a[j] + a[k] == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    } 
+
+    /**
+     * Reads in a sequence of integers from a file, specified as a command-line argument;
+     * counts the number of triples sum to exactly zero; prints out the time to perform
+     * the computation.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args)  {
+
+        String[] arr = {"1Kints.txt", "2Kints.txt", "4Kints.txt", "8Kints.txt", "8ints.txt", "16Kints.txt", "32Kints.txt"};
+
+        for (int i = 0; i < 7; i++){
+
+            In in = new In(arr[i]);
+            int[] a = in.readAllInts();
+
+            final long startTime = System.nanoTime();
+            int count = count(a);
+            final long elapsedTime = System.nanoTime()-startTime;
+
+            System.out.println(arr[i] + " Count: " + count);
+            System.out.println("Time Taken for algorithm to run in nanoseconds: " + elapsedTime);
+            long milliseconds = TimeUnit.NANOSECONDS.toMillis(elapsedTime);
+            System.out.println("Time Taken for algorithm to run in milliseconds: " + milliseconds);
+
+        }
+
+
+
+              
+    } 
+} 
+
+
